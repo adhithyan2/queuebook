8727ef6b372a0c2db6e0d8468765c2db31ae495e
adhithyan2 <adhithiyanprabaharan@gmail.com>
2026-07-13 14:59:41 +0530

    Optimize login logo to 19KB JPG for fast loading

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -8,7 +8,7 @@ export default function AuthLayout() {
         <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAwIiBoZWlnaHQ9IjYwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZGVmcz48cGF0dGVybiBpZD0iZ3JpZCIgd2lkdGg9IjQwIiBoZWlnaHQ9IjQwIiBwYXR0ZXJuVW5pdHM9InVzZXJTcGFjZU9uVXNlIj48cGF0aCBkPSJNIDQwIDAgTCAwIDAgMCA0MCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSJyZ2JhKDI1NSwyNTUsMjU1LDAuMDUpIiBzdHJva2Utd2lkdGg9IjEiLz48L3BhdHRlcm4+PC9kZWZzPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9InVybCgjZ3JpZCkiLz48L3N2Zz4=')] opacity-50" />
         <div className="relative z-10 text-center max-w-md">
           <img
-            src="/login-logo.png"
+            src="/login-logo.jpg"
             alt="QueueBook"
             className="w-[320px] max-w-full h-auto mx-auto"
             style={{ marginTop: '40px', marginBottom: '32px' }}
